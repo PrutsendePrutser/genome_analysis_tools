a5574607401bac5a72142f69496b14ff7ac4f15c
Brian Aarts <bmtc.aarts@gmail.com>
2016-02-22 13:45:41 +0100

    Added docstrings to blast_utils.py

--- genome_analysis_tools/utils/blast_utils.py
+++ genome_analysis_tools/utils/blast_utils.py
@@ -1,4 +1,15 @@
+"""Utils for BLAST, to easily get the commandline string, retrieve file paths
+etcetera
+"""
+
+
 def create_makeblastdb_input_string(filepath_list):
+    """
+    Create a makeblastdb string, that can be passed to the commandline utils,
+    to create the BLAST databases
+    :param filepath_list: List of files to include in the BLAST DB
+    :return: string that contains the commandline argument
+    """
     # Wrap each filename in double quotes, this is required by makeblastdb in case there are
     # any spaces in the filepath
     makeblastdb_input_list = ['"{}"'.format(filepath) for filepath in filepath_list]
