4c64a1f1fae1cb3bc855b434d82822235231ff67
Brian Aarts <bmtc.aarts@gmail.com>
2016-02-22 13:49:50 +0100

    Add .pylintrc file

--- genome_analysis_tools/models.py
+++ genome_analysis_tools/models.py
@@ -1,4 +1,7 @@
 # genome_analysis_tools/models.py
 
+"""This requires a docstring now, because we added a pre-commit hook
+"""
+
 # Not sure what this does yet. It will probably contain the data models for each app
 pass
